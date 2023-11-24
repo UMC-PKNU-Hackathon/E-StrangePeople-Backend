fdc20d9f073a62aba63567f4157f74aca141d7ca
최민석 <120546936+Minseok-git@users.noreply.github.com>
2023-11-24 23:25:47 +0900

    Add files via upload

---
+++ E-StrangePeople-Backend/hackathon/spring/src/main/java/com/hackathon/spring/common/Constants.java
@@ -0,0 +1,10 @@
+package com.hackathon.spring.common;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Constants {
+    public static final String FIREBASE_SDK_JSON ="/Users/choeminseok/Desktop/hackathon/E-StrangePeople-Backend/hackathon/spring/src/main/resources/serviceAccountKey.json";//copy the sdk-config file root address, if its in root ,filename is enough
+    public static final String FIREBASE_BUCKET = "hackathon-e885a.appspot.com";//enter your bucket name
+    public static final String FIREBASE_PROJECT_ID ="hackathon-e885a";//enter your project id
+}
---
+++ E-StrangePeople-Backend/hackathon/spring/src/main/java/com/hackathon/spring/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.hackathon.spring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ArticleController {
+
+}
